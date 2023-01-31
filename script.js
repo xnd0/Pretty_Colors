d0cd97c9695a6a20a69d97cc6c20b2adbf0c15da
xndcmd <xndcmd@gmail.com>
2023-01-31 15:29:42 -0800

    added black + white screen display to color switch cycle

--- script.js
+++ script.js
@@ -5,50 +5,35 @@
 
 const el = document.querySelector(".el");
 const display = document.querySelector(".display")
+
 let s = 1;
 
 el.addEventListener("click", () => {
-    // el.classList.remove(`state-${s + 1}`);
-    //   s = (s + 1) % 3;
-    //   el.classList.add(`state-${s + 1}`);
 
-
-    if (s===1) {
+    if (s === 1) {
         display.classList.replace("colorbox", "colorbox-radial");
         s++;
-        console.log(s);
         return;
-    }
+    };
 
-    if (s===2) {
-        display.classList.replace("colorbox-radial", "colorbox");
-        s--;
-        console.log(s)
+    if (s === 2) {
+        display.classList.replace("colorbox-radial", "screen-black");
+        s++;
         return;
-    }
-    
+    };
 
-    // display.classList.remove("colorbox");
-    // display.classList.add("screen-black")
+    if (s === 3) {
+        display.classList.replace("screen-black", "screen-white");
+        s++;
+        return;
+    };
 
-    // colorbox.classList.add("screen-white")
-    // el.classList.replace("screen-white", "screen-black") ||
-    // el.classList.replace("screen-black", "colors-one");
+    if (s === 4) {
+        display.classList.replace("screen-white", "colorbox");
+        s = 1;
+        return;
+    };
 
     console.log("clicked");
 
 });
-
-
-
-// const buttonOne = document.querySelector('.button-one');
-
-// function switchColor() {
-
-// }
-
-
-
-// // buttonOne.classList.add('new-class');
-
-// buttonOne.addEventListener('click', switchColor);
